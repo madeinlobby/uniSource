f21462326f8f6b8f4e943239259e10d793e12a1e
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-08-24 17:58:40 +0430

    initialize

---
+++ src/main/java/com/unisource/universitysource/UniversitysourceApplication.java
@@ -0,0 +1,13 @@
+package com.unisource.universitysource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitysourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversitysourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unisource/universitysource/UniversitysourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisource.universitysource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitysourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
