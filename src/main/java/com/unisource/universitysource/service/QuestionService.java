39cb1a9d32d70f31544bac9ed9895df005110f50
amirhosseinbagheri <amirhossein.b01@yahoo.com>
2020-08-26 16:50:09 +0430

    services done

--- src/main/java/com/unisource/universitysource/service/QuestionService.java
+++ src/main/java/com/unisource/universitysource/service/QuestionService.java
@@ -1,6 +1,20 @@
 package com.unisource.universitysource.service;
 
-public class QuestionService {
+import com.unisource.universitysource.model.Question;
+import com.unisource.universitysource.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class QuestionService {
+    @Autowired
+    QuestionRepository questionRepository;
+    public Question getById(int id){
+       return questionRepository.findById(id).get();
+    }
+    public void edit(Question question){
+        questionRepository.save(question);
+    }
+    public void deleteById(int id){
+        questionRepository.deleteById(id);
+    }
 
 }
