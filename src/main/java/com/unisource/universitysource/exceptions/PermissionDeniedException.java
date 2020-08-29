21c42b914e4fb395ab78a6ce1795b02f810c194d
amirhosseinbagheri <amirhossein.b01@yahoo.com>
2020-08-30 00:37:46 +0430

    felan ina to zehname

---
+++ src/main/java/com/unisource/universitysource/exceptions/PermissionDeniedException.java
@@ -0,0 +1,7 @@
+package com.unisource.universitysource.exceptions;
+
+public class PermissionDeniedException extends Exception {
+    public PermissionDeniedException(String message) {
+        super(message);
+    }
+}
