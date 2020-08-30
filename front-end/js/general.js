8f7203b49a2b59ed0b124fe6ff516780c61940a2
Ali <a80.abbasi@yahoo.com>
2020-08-30 19:39:45 +0430

    new footer

---
+++ front-end/js/general.js
@@ -0,0 +1,9 @@
+const mainTag = document.querySelector('main');
+if(mainTag.style.height < window.innerHeight) {
+    mainTag.style.height = window.innerHeight + 'px';
+}
+window.addEventListener("resize", function () {
+    if(mainTag.style.height < window.innerHeight) {
+        mainTag.style.height = window.innerHeight + 'px';
+    }
+});
